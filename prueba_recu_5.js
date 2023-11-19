d361d8754336cf2d91889c2b3501d7ace6688211
Brendareyes03 <103964013+Brendareyes03@users.noreply.github.com>
2023-11-18 21:48:02 -0300

    Update prueba_recu_5.js

--- prueba_recu_5.js
+++ prueba_recu_5.js
@@ -117,3 +117,41 @@ class Juego {
     }
   }
 }
+class Jugador {
+  constructor() { //propiedades del jugador
+    this.x = width / 2;
+    this.y = height / 2;
+    this.ancho = 40;
+    this.velocidad = 5;
+  }
+
+  mostrar() {
+    image(imagenes[1], this.x, this.y, this.ancho, this.ancho);
+  }
+//movimiento del jugador
+  moverArriba() {
+    this.y -= this.velocidad;
+    this.y = constrain(this.y, 0, height - this.ancho / 2);
+  }
+
+  moverAbajo() {
+    this.y += this.velocidad;
+    this.y = constrain(this.y, 0, height - this.ancho / 2);
+  }
+
+  mover() {
+    if (keyIsPressed) {
+      if (keyCode === UP_ARROW) {
+        this.moverArriba();
+      } else if (keyCode === DOWN_ARROW) {
+        this.moverAbajo();
+      }
+    }
+  }
+
+//verificasi hay colision del jugador con obstacilo
+  colision(o) {
+    let distancia = dist(this.x, this.y, o.x, o.y);
+    return distancia < (this.ancho + o.ancho) / 4;
+  }
+}
