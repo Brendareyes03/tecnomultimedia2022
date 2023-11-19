612e820ac80d5ab0afaf69cd541f1437d9ae7e24
Brendareyes03 <103964013+Brendareyes03@users.noreply.github.com>
2023-11-18 21:48:23 -0300

    Update prueba_recu_5.js

--- prueba_recu_5.js
+++ prueba_recu_5.js
@@ -155,3 +155,23 @@ class Jugador {
     return distancia < (this.ancho + o.ancho) / 4;
   }
 }
+
+class Obstaculo { //propiedades del obstaculo
+  constructor() {
+    this.x = width;
+    this.y = random(height);
+    this.ancho = random(30, 50);
+    this.velocidad = random(1, 3);
+  }
+
+ mostrar() {
+    image(imagenes[0], this.x, this.y, this.ancho, this.ancho);
+ }
+  mover() { //mov del obstaculo
+    this.x -= this.velocidad;
+  }
+ //verifica si salio de la pantalla
+  salirPantalla() {
+    return this.x < 0;
+  }
+}
